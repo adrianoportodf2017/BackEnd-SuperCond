757a40b9acf2be5e8c259b79c7aa6a17c571570f
adriano alves <33916451+adrianoportodf2017@users.noreply.github.com>
2023-11-12 13:27:13 -0300

    me salvando ;(

--- app/Http/Controllers/ClassifiedsController.php
+++ app/Http/Controllers/ClassifiedsController.php
@@ -324,7 +324,7 @@ class ClassifiedsController extends Controller
             }
         }
         foreach ($files as  $key) {
-            $arquivo = $key->store('public/classifieds/' . $request->input('user_id'));
+            $arquivo = $key->store('public/classifieds/' .   $classified->user_id);
             $url = asset(Storage::url($arquivo));
             $midia = new Midia([
                 'title' => 'Classificados',
@@ -338,6 +338,30 @@ class ClassifiedsController extends Controller
             // Salvar o documento no banco de dados
             try {
                 $classified->midias()->save($midia);
+                $midias = $classified->midias;
+                // Obtém a URL base para os ícones
+                $iconBaseUrl = asset('assets/icons/');
+                // Itere sobre cada item na coleção e adicione o tipo de arquivo e o ícone com URL completa
+                foreach ($midias as $midia) {
+                    $fileExtension = strtolower(pathinfo($midia->url, PATHINFO_EXTENSION));
+                    if (in_array($fileExtension, ['jpg', 'jpeg', 'png', 'gif'])) {
+                        $midia->type = 'imagem';
+                        $midia->icon = $midia->url;
+                    } elseif ($fileExtension === 'pdf') {
+                        $midia->type = 'pdf';
+                        $midia->icon = $iconBaseUrl . '/pdf.png';
+                    } elseif (in_array($fileExtension, ['doc', 'docx'])) {
+                        $midia->type = 'word';
+                        $midia->icon = $iconBaseUrl . '/word.png';
+                    } elseif (in_array($fileExtension, ['xls', 'xlsx'])) {
+                        $midia->type = 'word';
+                        $midia->icon = $iconBaseUrl . '/excel.png';
+                    } else {
+                        $midia->type = 'outro';
+                        $midia->icon = $iconBaseUrl . '/outros.png';
+                    }
+                }
+                $classified['midias'] = $midias;
             } catch (Exception $e) {
                 // Tratar o erro
                 return response()->json([
