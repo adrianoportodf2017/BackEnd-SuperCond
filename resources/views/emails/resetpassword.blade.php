fc41a712b2e53e6ea2e58cb39db1e8a41b832c2a
adriano <adrianobr00@gmail.com>
2024-07-17 14:13:29 -0300

    atualizando

---
+++ resources/views/emails/resetpassword.blade.php
@@ -0,0 +1,19 @@
+@component('mail::message')
+# Olá!
+
+Esqueceu sua senha?
+
+@component('mail::button', ['url' => $url])
+Clique para redefinir
+@endcomponent
+
+Obrigado por usar nossa aplicação!
+
+Atenciosamente,<br>
+{{ config('app.name') }}
+
+@component('mail::subcopy')
+Se você estiver tendo problemas para clicar no botão "Clique para redefinir", copie e cole a URL abaixo no seu navegador web: [{{ $url }}]({{ $url }})
+@endcomponent
+
+@endcomponent
