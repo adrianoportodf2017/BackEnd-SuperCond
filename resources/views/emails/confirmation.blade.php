2edf887f1ffac76f7c28cd73d405b0ffd272fbc0
adriano <adrianobr00@gmail.com>
2024-08-07 18:06:57 -0300

    atuaçizanod

--- resources/views/emails/confirmation.blade.php
+++ resources/views/emails/confirmation.blade.php
@@ -6,10 +6,13 @@
     <title>Recebemos sua mensagem</title>
 </head>
 <body>
+<div style="background-color: white;border: 2px solid #0f0870;box-shadow: 20px -13px 1px 1px #0f0870;
+        width: fit-content;padding: 1rem 1rem;font-family: system-ui;">
+            <h4 style="text-align: center; font-size: large;"> 
     <p>Olá {{ $data['name'] }},</p>
 
     <p>Recebemos sua mensagem e entraremos em contato em breve.</p>
-
+</div>
     <p>Atenciosamente,<br>
     Agência Tecnet</p>
 </body>
