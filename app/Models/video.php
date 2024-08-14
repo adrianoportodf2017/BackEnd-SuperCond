f22ef81b61b0a7ac6bbc6a1faef16947e4f5d718
adriano <adrianobr00@gmail.com>
2024-08-14 17:50:49 -0300

    atuaçizando video

--- app/Models/video.php
+++ app/Models/video.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Video extends Model
+class Videos extends Model
 {
     use HasFactory;
+    protected $table = 'videos';
+
 }
