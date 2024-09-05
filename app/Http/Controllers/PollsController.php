a18c6418c3ff5d1f3173fd64714cb5462615a709
adriano <adrianobr00@gmail.com>
2024-09-05 17:03:43 -0300

    adicionando status

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -16,7 +16,7 @@ class PollsController extends Controller
 {
     public function getAll()
     {
-        $polls = Poll::all();
+        $polls = Poll::with('questions.answers')->get();
 
 
         // Retornar uma mensagem de erro se não houver ocorrencias
@@ -227,7 +227,12 @@ class PollsController extends Controller
 
         // Excluir todas as perguntas relacionadas à enquete e suas respostas
         try {
-          
+            $questions = QuestionPoll::where('poll_id', $poll->id)->get();
+            foreach ($questions as $question) {
+                $question->answers()->delete();
+                $question->delete();
+            }
+
             $poll->delete();
             $fileDelete = $poll->thumb_file;
             Storage::delete($fileDelete);
@@ -259,5 +264,244 @@ class PollsController extends Controller
             return $request->input();
         }
     }
-   
+    public function getQuestionsAll($id)
+    {
+        // Implemente a lógica para listar todas as perguntas de uma enquete
+        $questions = QuestionPoll::where('poll_id', $id)->get();
+
+        if (!$questions) {
+            return response()->json([
+                'error' => "Galeria não encontrado",
+                'code' => 404,
+            ], 404);
+        }
+        return response()->json([
+            'error' => '',
+            'list' => json_decode($questions),
+            // Outros dados de resultado aqui...
+        ], 200);
+    }
+
+    public function getQuestionById($id)
+    {
+        // Implemente a lógica para obter uma pergunta específica por ID
+        // Implemente a lógica para obter uma enquete específica por ID
+
+        $question = QuestionPoll::where('id', $id)->with('answers')->first();
+
+        if (!$question) {
+            return response()->json([
+                'error' => "Pergunta não encontrada",
+                'code' => 404,
+            ], 404);
+        }
+        return response()->json([
+            'error' => '',
+            'list' => $question,
+            // Outros dados de resultado aqui...
+        ], 200);
+    }
+
+    public function insertQuestion(Request $request, $id)
+    {
+        // Implemente a lógica para criar uma nova pergunta relacionada a uma enquete
+
+        //  return var_dump($request->file()); die;
+
+        // Validar os dados da requisição
+
+        $newQuestion = new QuestionPoll();
+
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|min:2',
+            'type' =>  'required|min:2',
+
+        ]);
+
+        // Retornar uma mensagem de erro se a validação falhar
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+                'code' => 422,
+            ], 422);
+        }
+
+        $newQuestion->title = $request->input('title');
+        $newQuestion->content = $request->input('content');
+        $newQuestion->type = $request->input('type');
+        $newQuestion->poll_id = $id;
+
+
+        // Salvar o documento no banco de dados
+        try {
+            $newQuestion->save();
+        } catch (Exception $e) {
+            // Tratar o erro
+            return response()->json([
+                'error' => 'Erro ao salvar Pergunta!',
+                'detail' => $e->getMessage(),
+                'code' => 500,
+            ], 500);
+        }
+
+
+        return response()->json([
+            'error' => '',
+            'success' => true,
+            'list' => $newQuestion,
+        ], 201);
+    }
+
+
+
+    public function updateQuestion(Request $request, $id)
+    {
+        // Implemente a lógica para atualizar uma pergunta existente
+
+        $question =  QuestionPoll::where('id', $id)->first();
+
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|min:2',
+            'type' =>  'required|min:2',
+
+        ]);
+
+        // Retornar uma mensagem de erro se a validação falhar
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+                'code' => 422,
+            ], 422);
+        }
+
+        $question->title = $request->input('title');
+        $question->content = $request->input('content');
+        $question->type = $request->input('type');
+        //$question->poll_id = $id;
+
+
+        // Salvar o documento no banco de dados
+        try {
+            $question->save();
+        } catch (Exception $e) {
+            // Tratar o erro
+            return response()->json([
+                'error' => 'Erro ao salvar Pergunta!',
+                'detail' => $e->getMessage(),
+                'code' => 500,
+            ], 500);
+        }
+
+
+        return response()->json([
+            'error' => '',
+            'success' => true,
+            'list' => $question,
+        ], 201);
+    }
+    public function deleteQuestion($id)
+    {
+        // Buscar a pergunta a ser deletada
+        $question = QuestionPoll::find($id);
+
+        // Verificar se a pergunta existe
+        if (!$question) {
+            return response()->json(['error' => 'Pergunta Inexistente', 'code' => 404], 404);
+        }
+
+        // Excluir a pergunta e suas respostas associadas
+        try {
+
+            $question->answers()->delete();
+            $question->delete();
+        } catch (Exception $e) {
+            // Tratar o erro
+            return response()->json([
+                'error' => 'Erro ao Deletar!',
+                'detail' => $e->getMessage(),
+                'code' => 500,
+            ], 500);
+        }
+
+
+        return response()->json(['error' => '', 'success' => true], 200);
+    }
+
+
+    public function getAnswersByPoll($pollId)
+    {
+        // Recupere a enquete específica com suas perguntas e respostas
+        $poll = Poll::with('questions.answers')
+            ->find($pollId);
+
+        // Verifique se a enquete existe
+        if (!$poll) {
+            return response()->json([
+                'error' => 'Enquete não encontrada'
+            ],  404);
+        }
+
+        // Extraia apenas as respostas da enquete
+        $answers = [];
+        foreach ($poll->questions as $question) {
+            $answers = array_merge($answers, $question->answers->toArray());
+        }
+
+        // Retornar uma resposta de sucesso
+        return response()->json([
+            'error' => '',
+            'list' => $answers,
+        ], 200);
+    }
+
+    public function insertAnswer(Request $request, $id)
+    {
+
+
+        //Realizar Voto
+        // Implemente a lógica para criar uma nova resposta relacionada a uma enquete 
+
+        $newAnswer = new VotePoll();
+
+
+        $validator = Validator::make($request->all(), [
+            'answer' => 'required',
+            'user_id' =>  'required',
+
+        ]);
+
+        // Retornar uma mensagem de erro se a validação falhar
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+                'code' => 422,
+            ], 422);
+        }
+
+        $newAnswer->answer = $request->input('answer');
+        $newAnswer->user_id = $request->input('user_id');
+        $newAnswer->question_poll_id = $id;
+
+
+        // Salvar o documento no banco de dados
+        try {
+            $newAnswer->save();
+        } catch (Exception $e) {
+            // Tratar o erro
+            return response()->json([
+                'error' => 'Erro ao salvar Resposta!',
+                'detail' => $e->getMessage(),
+                'code' => 500,
+            ], 500);
+        }
+
+
+        return response()->json([
+            'error' => '',
+            'success' => true,
+            'list' => $newAnswer,
+        ], 201);
+    }
 }
